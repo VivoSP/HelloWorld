29f3e1834850fa66d7237439edabbbb7df2b038b
ArianneAfo <135263016+ArianneAfo@users.noreply.github.com>
2023-06-01 15:51:12 -0300

    ultimo

--- HelloWordSP_1/HelloWordSP_1.cs
+++ HelloWordSP_1/HelloWordSP_1.cs
@@ -68,7 +68,7 @@ namespace HelloWordSP_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation("Hello SP!");
+			engine.GenerateInformation("Hello World!");
 		}
 	}
 }
