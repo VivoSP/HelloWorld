735b599f68be61efb009686cccea874236c9bfe5
Engenharia <Engenharia@ENG_SDB_02>
2023-06-01 15:19:36 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWordSP_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloWordSP_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("HelloWordSP_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("CF18DC9F-2F35-43E8-8696-82F4EC320B04")]
+[assembly: AssemblyVersion("1.0.0.0")]
